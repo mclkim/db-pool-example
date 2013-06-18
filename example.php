6ff479366af9779bd828a91e99ed80f681674e58
justinmealey <sglitch5@gmail.com>
2013-06-18 04:25:46 -0600

    Create example.php

---
+++ example.php
@@ -0,0 +1,10 @@
+<?php
+require_once('DB.php');
+
+$db = DB::getFactory();
+// Columns are given seperately in order to append only the data you want from the data object
+$db->insert((object)array('table' => 'foo', 'columns' => 'bar1,bar2', 'data' => (object)array('bar1' => 'item1', 'bar2' => 'item2')));
+$db->update((object)array('table' => 'foo', 'columns' => 'bar1,bar2', 'where' => 'bar1=:example', 'data' => (object)array('example' => 'item1', 'bar1' => 'item1changed', 'bar2' => 'item2changed')));
+$db->delete((object)array('table' => 'foo', 'where' => 'bar1=:example', 'data' => array(':example' => 'item1changed')));
+
+?>
