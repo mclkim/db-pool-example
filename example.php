89b7778970fac440fa5236b5805113d1c60afb1f
justinmealey <sglitch5@gmail.com>
2013-06-18 03:32:45 -0700

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php
 require_once('DB.php');
 
-$db = DB::getFactory();
+$db = DB::getFactory()->getConnection('write');
 // Columns are given seperately in order to append only the data you want from the data object
 $db->insert((object)array('table' => 'foo', 'columns' => 'bar1,bar2', 'data' => (object)array('bar1' => 'item1', 'bar2' => 'item2')));
 $db->update((object)array('table' => 'foo', 'columns' => 'bar1,bar2', 'where' => 'bar1=:example', 'data' => (object)array('example' => 'item1', 'bar1' => 'item1changed', 'bar2' => 'item2changed')));
